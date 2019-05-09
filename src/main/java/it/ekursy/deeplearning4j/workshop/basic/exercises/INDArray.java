3d0c56cfa29db66f9de691a1b73854ff51027f74
Marek Bedkowski <marek@bedkowski.pl>
2019-05-09 17:01:52 +0200

    Add initial structure for INDArray exercises.

---
+++ src/main/java/it/ekursy/deeplearning4j/workshop/basic/exercises/INDArray.java
@@ -0,0 +1,16 @@
+package it.ekursy.deeplearning4j.workshop.basic.exercises;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.nd4j.linalg.factory.Nd4j;
+
+public class INDArray {
+
+    private static final Logger LOG = LogManager.getLogger();
+
+    public static void main(String[] args)
+    {
+        var zeros = Nd4j.zeros( 2, 2 );
+    }
+}
